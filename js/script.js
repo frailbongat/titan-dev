95284dea8029c5baeed43dabed8f44f3f1cc9219
Reign Hart G. Cabasag <31159755+reignO7@users.noreply.github.com>
2020-03-03 16:45:47 +0800

    initial design

---
+++ js/script.js
@@ -0,0 +1,147 @@
+const ourTeam = document.querySelectorAll(".grid-item");
+const ourTeamTitle = document.querySelector(".ourteam-title");
+const ourTeamDescription = document.querySelector(".ourteam-description");
+
+ourTeam[0].addEventListener("mouseover", function() {
+    ourTeamTitle.innerHTML =
+    "Mark Sayre";
+
+    ourTeamDescription.innerHTML =
+    "Team Lead / QA Tester";
+});
+
+ourTeam[0].addEventListener("mouseout", function() {
+    ourTeamTitle.innerHTML =
+    "OUR TEAM";
+
+    ourTeamDescription.innerHTML =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+});
+
+ourTeam[1].addEventListener("mouseover", function() {
+    ourTeamTitle.innerHTML =
+    "Keny Pecorro";
+
+    ourTeamDescription.innerHTML =
+    "Web Developer";
+});
+
+ourTeam[1].addEventListener("mouseout", function() {
+    ourTeamTitle.innerHTML =
+    "OUR TEAM";
+
+    ourTeamDescription.innerHTML =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+});
+
+ourTeam[2].addEventListener("mouseover", function() {
+    ourTeamTitle.innerHTML =
+    "Justine Macarat";
+
+    ourTeamDescription.innerHTML =
+    "Web Developer";
+});
+
+ourTeam[2].addEventListener("mouseout", function() {
+    ourTeamTitle.innerHTML =
+    "OUR TEAM";
+
+    ourTeamDescription.innerHTML =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+});
+
+ourTeam[3].addEventListener("mouseover", function() {
+    ourTeamTitle.innerHTML =
+    "Ched Castillon";
+
+    ourTeamDescription.innerHTML =
+    "Team Lead / Software Developer";
+});
+
+ourTeam[3].addEventListener("mouseout", function() {
+    ourTeamTitle.innerHTML =
+    "OUR TEAM";
+
+    ourTeamDescription.innerHTML =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+});
+
+ourTeam[4].addEventListener("mouseover", function() {
+    ourTeamTitle.innerHTML =
+    "Kim Revil";
+
+    ourTeamDescription.innerHTML =
+    "Software Developer";
+});
+
+ourTeam[4].addEventListener("mouseout", function() {
+    ourTeamTitle.innerHTML =
+    "OUR TEAM";
+
+    ourTeamDescription.innerHTML =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+});
+
+ourTeam[5].addEventListener("mouseover", function() {
+    ourTeamTitle.innerHTML =
+    "Frail Bongat";
+
+    ourTeamDescription.innerHTML =
+    "Software Developer";
+});
+
+ourTeam[5].addEventListener("mouseout", function() {
+    ourTeamTitle.innerHTML =
+    "OUR TEAM";
+
+    ourTeamDescription.innerHTML =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+});
+
+ourTeam[6].addEventListener("mouseover", function() {
+    ourTeamTitle.innerHTML =
+    "Raphael Guray";
+
+    ourTeamDescription.innerHTML =
+    "Software Developer";
+});
+
+ourTeam[6].addEventListener("mouseout", function() {
+    ourTeamTitle.innerHTML =
+    "OUR TEAM";
+
+    ourTeamDescription.innerHTML =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+});
+
+ourTeam[7].addEventListener("mouseover", function() {
+    ourTeamTitle.innerHTML =
+    "Reign Cabasag";
+
+    ourTeamDescription.innerHTML =
+    "Software Developer";
+});
+
+ourTeam[7].addEventListener("mouseout", function() {
+    ourTeamTitle.innerHTML =
+    "OUR TEAM";
+
+    ourTeamDescription.innerHTML =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+});
+
+ourTeam[8].addEventListener("mouseover", function() {
+    ourTeamTitle.innerHTML =
+    "Nicole Bugas";
+
+    ourTeamDescription.innerHTML =
+    "QA Tester";
+});
+
+ourTeam[8].addEventListener("mouseout", function() {
+    ourTeamTitle.innerHTML =
+    "OUR TEAM";
+
+    ourTeamDescription.innerHTML =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+});
