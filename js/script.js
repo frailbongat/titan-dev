b2d30cfda796ac01d358a5ead1d4ef64df537908
arn\raphaelguray <raphael.guray@arnlea.com>
2020-03-03 17:49:15 +0800

    change the logos and background color

--- js/script.js
+++ js/script.js
@@ -4,76 +4,82 @@ const clientsDescription = document.querySelector(".clients-description");
 
 clientsProject[0].addEventListener("mouseover", function() {
   clientsTitle.innerHTML =
-    '<img src="img/enkelt.png" width="400px" height="300px">';
+    '<img src="img/enkelt.png" width="400px" height="400px">';
 
   clientsDescription.innerHTML =
-    "Designed and developed a web-based asset maintenance & management system for their clients";
+    "Designed and developed a web-based asset maintenance & management system for their clients.";
 });
 clientsProject[0].addEventListener("mouseout", function() {
   clientsTitle.innerHTML = "Clients & Project";
-  clientsDescription.innerHTML = " ";
+  clientsDescription.innerHTML =
+    "We don't want to push our ideas on to customers, we simply want to make what they want.";
 });
 
 clientsProject[1].addEventListener("mouseover", function() {
   clientsTitle.innerHTML =
-    '<img src="img/bluewater.png" width="400px" height="300px">';
+    '<img src="img/bluewater.png" width="400px" height="400px">';
 
   clientsDescription.innerHTML =
-    "Developed a web-based asset management system for their oil and gas rigs";
+    "Developed a web-based asset management system for their oil and gas rigs.";
 });
 
 clientsProject[1].addEventListener("mouseout", function() {
   clientsTitle.innerHTML = "Clients & Project";
-  clientsDescription.innerHTML = " ";
+  clientsDescription.innerHTML =
+    "We don't want to push our ideas on to customers, we simply want to make what they want.";
 });
 
 clientsProject[2].addEventListener("mouseover", function() {
   clientsTitle.innerHTML =
-    '<img src="img/arnlealogo.png" width="400px" height="300px">';
+    '<img src="img/arnlea.png" width="400px" height="400px">';
 
   clientsDescription.innerHTML =
-    "Assisted in developing operations management and material management systems that runs in mobile, desktop and web";
+    "Assisted in developing operations management and material management systems that runs in mobile, desktop and web.";
 });
 
 clientsProject[2].addEventListener("mouseout", function() {
   clientsTitle.innerHTML = "Clients & Project";
-  clientsDescription.innerHTML = " ";
+  clientsDescription.innerHTML =
+    "We don't want to push our ideas on to customers, we simply want to make what they want.";
 });
 
 clientsProject[3].addEventListener("mouseover", function() {
   clientsTitle.innerHTML =
-    '<img src="img/greenkleen.png" width="400px" height="300px">';
+    '<img src="img/greenkleen.png" width="400px" height="400px">';
 
   clientsDescription.innerHTML =
-    "Designed and developed GreenKleen's official website";
+    "Designed and developed GreenKleen's official website.";
 });
 
 clientsProject[3].addEventListener("mouseout", function() {
   clientsTitle.innerHTML = "Clients & Project";
-  clientsDescription.innerHTML = " ";
+  clientsDescription.innerHTML =
+    "We don't want to push our ideas on to customers, we simply want to make what they want.";
 });
 
 clientsProject[4].addEventListener("mouseover", function() {
   clientsTitle.innerHTML =
-    '<img src="img/herbidoor.png" width="400px" height="300px">';
+    '<img src="img/herbidoor.png" width="400px" height="400px">';
 
-  clientsDescription.innerHTML = "Commissioned to design the company's logo ";
+  clientsDescription.innerHTML = "Commissioned to design the company's logo.";
 });
 
 clientsProject[4].addEventListener("mouseout", function() {
   clientsTitle.innerHTML = "Clients & Project";
-  clientsDescription.innerHTML = " ";
+  clientsDescription.innerHTML =
+    "We don't want to push our ideas on to customers, we simply want to make what they want.";
 });
 
 clientsProject[5].addEventListener("mouseover", function() {
   clientsTitle.innerHTML =
-    '<img src="img/amandarose.png" width="400px" height="300px">';
+    '<img src="img/amandarose.png" width="400px" height="400px">';
 
   clientsDescription.innerHTML =
-    "Commissioned to design company's logo and graphics for their social media accounts";
+    "Commissioned to design company's logo and graphics for their social media accounts.";
 });
 
 clientsProject[5].addEventListener("mouseout", function() {
   clientsTitle.innerHTML = "Clients & Project";
-  clientsDescription.innerHTML = " ";
+  clientsDescription.innerHTML =
+    "We don't want to push our ideas on to customers, we simply want to make what they want.";
 });
